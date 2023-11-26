7e9a3ca0cf949cc954d6cb000d07dbb4dd09fdf8
chilux00 <chiingzhang@gmail.com>
2023-11-25 17:52:52 -0800

    cleaning data

--- pilots/pilot2_graphs.R
+++ pilots/pilot2_graphs.R
@@ -6,12 +6,16 @@ library(tidyverse)
 library(ggplot2)
 library(cowplot)
 
+# Initial read of csv data from Multiquant
 pilot2_data <- read.csv("data/sep27data_DCMEA3buffer.csv")
 pilot2_data
 
+# Removing NA values
+pilot2_data_cleaned <- pilot2_data %>% drop_na(area)
+
 pilot2_bar <- pilot2_data %>%
-  ggplot(aes(x = buffer,),
+  ggplot(aes(x = buffer),
          color = reagent) +
-  geom_bar() 
+  geom_bar(stat = identity) 
 
 pilot2_bar  
