5eff6f8880ccc0663fb023599d3f288b89086ef5
chilux00 <chiingzhang@gmail.com>
2023-11-25 18:33:28 -0800

    i give up

--- pilots/pilot2_graphs.R
+++ pilots/pilot2_graphs.R
@@ -7,11 +7,15 @@ library(ggplot2)
 library(cowplot)
 
 # Initial read of csv data from Multiquant
-pilot2_data <- read.csv("data/sep27data_DCMEA3buffer.csv")
+pilot2_data <- read.csv("data/sep27data_DCMEA3buffer.csv", na.rm = TRUE)
 pilot2_data
 
 # Removing NA values
-pilot2_data_cleaned <- pilot2_data %>% drop_na(area)
+pilot2_data_cleaned <- pilot2_data %>%
+  drop_na(area) %>%
+  as.data.frame(complete.cases(pilot2_data$area))
+
+pilot2_data_cleaned
 
 pilot2_bar <- pilot2_data_cleaned %>%
   ggplot(aes(x = buffer, 
@@ -24,7 +28,8 @@ pilot2_bar <- pilot2_data_cleaned %>%
   labs(title = "Pilot 2: Dichloromethane and Ethyl Acetate extraction of
        steroids from Qiagen, Monarch and Zymo RNA Lysis Buffers",
        x = "Buffer Kit",
-       y = "Area") +
+       y = "Area"
+       ) +
   
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5, size = 12))  
