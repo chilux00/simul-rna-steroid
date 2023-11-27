1733768cfa26fe0b455442973e17bc15caba6c1d
chilux00 <chiingzhang@gmail.com>
2023-11-27 01:18:18 -0800

    updated file csv

--- pilots/pilot3_graphs.R
+++ pilots/pilot3_graphs.R
@@ -11,3 +11,19 @@ library(dplyr)
 # Initial read of csv data from Multiquant
 pilot3_data <- read.csv("data/oct22data_DCMdryvols.csv")
 pilot3_data
+
+# Cleaning data types
+pilot3_data_cleaned <- pilot3_data %>%
+  mutate(area_p4 = as.numeric(area_p4)) %>% 
+  mutate(area_f = as.numeric(area_f)) %>% 
+  mutate(area_b = as.numeric(area_b)) %>% 
+  mutate(reagent = as.factor(reagent)) %>%
+  mutate(volume = as.factor(volume)) %>%
+  mutate(time = as.factor(time)) %>%
+  mutate(dilution = as.factor(dilution)) %>%
+  mutate(area_p4 = coalesce(area_p4, 0)) %>%  # Removal of NA values - keep?
+  mutate(area_f = coalesce(area_f, 0)) %>% 
+  mutate(area_b = coalesce(area_b, 0)) %>% 
+  as.data.frame()
+
+pilot1_data_cleaned
