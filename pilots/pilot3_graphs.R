6c3456a06795129e56f89bb4183cc5a3e1054da5
chilux00 <chiingzhang@gmail.com>
2023-11-27 01:12:06 -0800

    create data file for pilot 3

---
+++ pilots/pilot3_graphs.R
@@ -0,0 +1,9 @@
+## October 22 2023 - Pilot 1
+# Reagents: Ethyl Acetate (EA) and Dichloromethane (DCM)
+
+# Load necessary libraries
+library(tidyverse)
+library(ggplot2)
+library(cowplot)
+library(forcats)
+library(dplyr)
