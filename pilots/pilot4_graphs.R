c710b84bc0d76b05e6c54abc44d0cac951b9d7bf
chilux00 <chiingzhang@gmail.com>
2024-02-27 20:54:17 -0800

    new framework pilot 4

--- pilots/pilot4_graphs.R
+++ pilots/pilot4_graphs.R
@@ -10,27 +10,28 @@ library(dplyr)
 set.seed(1234)
 
 # Initial read of csv data from Multiquant
-pilot1_data <- read.csv("data/feb4data_5mlDCMEA.csv")
-pilot1_data
+pilot4_data <- read.csv("data/feb4data_5mlDCMEA.csv")
+pilot4_data
 
 # Cleaning data types
-pilot1_data_cleaned <- pilot1_data %>%
+pilot4_data_cleaned <- pilot4_data %>%
   mutate(area_p4 = as.numeric(area_p4)) %>% 
   mutate(area_f = as.numeric(area_f)) %>% 
   mutate(area_b = as.numeric(area_b)) %>% 
   mutate(reagent = as.factor(reagent)) %>%
+  mutate(buffer = as.factor(buffer)) %>%
   mutate(area_p4 = coalesce(area_p4, 0)) %>%  # Removal of NA values - keep?
   mutate(area_f = coalesce(area_f, 0)) %>% 
   mutate(area_b = coalesce(area_b, 0)) %>% 
   as.data.frame()
 
-pilot1_data_cleaned
+pilot4_data_cleaned
 
-# Plotting bar graph of pilot 1 area data per treatment
+# Plotting bar graph of pilot 4 area data per treatment
 # P4 Progesterone graph
-pilot1_bar_p4 <- 
+pilot4_bar_p4 <- 
   ggplot() + 
-  geom_bar(data = pilot1_data_cleaned,
+  geom_bar(data = pilot4_data_cleaned,
            aes(x = fct_inorder(reagent), 
                y = (area_p4),
                fill = as.factor(reagent)),
@@ -39,14 +40,14 @@ pilot1_bar_p4 <-
            width = 0.7,
            na.rm = TRUE
   ) +
-  geom_jitter(data = pilot1_data_cleaned,
+  geom_jitter(data = pilot4_data_cleaned,
               aes(x = reagent,
                   y = area_p4),
               stat = "identity",
               position = position_jitter(height = 0,
                                          width = 0)) +
   
-  labs(title = "Pilot 1: Dichloromethane and Ethyl Acetate extraction of
+  labs(title = "Pilot 4: 5mL Dichloromethane and Ethyl Acetate extraction of
        Progesterone (P4) Steroid Hormone from Qiagen RNA Kit Flowthrough",
        x = "Buffer Kit",
        y = "Area",
@@ -57,12 +58,12 @@ pilot1_bar_p4 <-
   theme(plot.title = element_text(hjust = 0.5, size = 10)
   )  
 
-pilot1_bar_p4
+pilot4_bar_p4
 
 # B Corticosterone graph
-pilot1_bar_b <- 
+pilot4_bar_b <- 
   ggplot() + 
-  geom_bar(data = pilot1_data_cleaned,
+  geom_bar(data = pilot4_data_cleaned,
            aes(x = fct_inorder(reagent), 
                y = (area_b),
                fill = as.factor(reagent)),
@@ -71,14 +72,14 @@ pilot1_bar_b <-
            width = 0.7,
            na.rm = TRUE
   ) +
-  geom_jitter(data = pilot1_data_cleaned,
+  geom_jitter(data = pilot4_data_cleaned,
               aes(x = reagent,
                   y = area_b),
               stat = "identity",
               position = position_jitter(height = 0,
                                          width = 0)) +
   
-  labs(title = "Pilot 1: Dichloromethane and Ethyl Acetate extraction of
+  labs(title = "Pilot 4: 5mL Dichloromethane and Ethyl Acetate extraction of
        Corticosterone (B) Steroid Hormone from Qiagen RNA Kit Flowthrough",
        x = "Buffer Kit",
        y = "Area",
@@ -89,12 +90,12 @@ pilot1_bar_b <-
   theme(plot.title = element_text(hjust = 0.5, size = 10)
   )  
 
-pilot1_bar_b
+pilot4_bar_b
 
 # F Cortisol graph
-pilot1_bar_f <- 
+pilot4_bar_f <- 
   ggplot() + 
-  geom_bar(data = pilot1_data_cleaned,
+  geom_bar(data = pilot4_data_cleaned,
            aes(x = fct_inorder(reagent), 
                y = (area_f),
                fill = as.factor(reagent)),
@@ -103,14 +104,14 @@ pilot1_bar_f <-
            width = 0.7,
            na.rm = TRUE
   ) +
-  geom_jitter(data = pilot1_data_cleaned,
+  geom_jitter(data = pilot4_data_cleaned,
               aes(x = reagent,
                   y = area_f),
               stat = "identity",
               position = position_jitter(height = 0,
                                          width = 0)) +
   
-  labs(title = "Pilot 1: Dichloromethane and Ethyl Acetate extraction of
+  labs(title = "Pilot 4: 5mL Dichloromethane and Ethyl Acetate extraction of
        Cortisol (F) Steroid Hormone from Qiagen RNA Kit Flowthrough",
        x = "Buffer Kit",
        y = "Area",
@@ -121,5 +122,5 @@ pilot1_bar_f <-
   theme(plot.title = element_text(hjust = 0.5, size = 10)
   )  
 
-pilot1_bar_f
+pilot4_bar_f
 
