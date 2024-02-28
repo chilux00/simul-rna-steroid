9a3ca8701c319d6821fdfba5c752c2ca08f0f5ec
chilux00 <chiingzhang@gmail.com>
2024-02-27 20:57:05 -0800

    change buffer label

--- pilots/pilot4_graphs.R
+++ pilots/pilot4_graphs.R
@@ -1,5 +1,5 @@
-## February 4, 2024 - Pilot 4
-# Reagents: Ethyl Acetate (EA) and Dichloromethane (DCM) - higher volumes
+## February 4 2024 - Pilot 4
+# Reagents: Ethyl Acetate (EA) and Dichloromethane (DCM) 5mL
 
 # Load necessary libraries
 library(tidyverse)
@@ -9,12 +9,25 @@ library(forcats)
 library(dplyr)
 set.seed(1234)
 
-# Initial read of csv data from Multiquant
-pilot4_data <- read.csv("data/feb4data_5mlDCMEA.csv")
-pilot4_data
+# Initial read of data from MultiQuant
+pilot4_indiv <- read.csv("data/feb4data_5mlDCMEA.csv")
+pilot4_data <- read.csv("data/feb4data_5mlDCMEA_averaged.csv")
 
 # Cleaning data types
 pilot4_data_cleaned <- pilot4_data %>%
+  mutate(avg_p4 = as.numeric(avg_p4)) %>% 
+  mutate(avg_f = as.numeric(avg_f)) %>% 
+  mutate(avg_b = as.numeric(avg_b)) %>% 
+  mutate(reagent = as.factor(reagent)) %>%
+  mutate(buffer = as.factor(buffer)) %>%
+  mutate(avg_p4 = coalesce(avg_p4, 0)) %>%  # Removal of NA values - keep?
+  mutate(avg_f = coalesce(avg_f, 0)) %>% 
+  mutate(avg_b = coalesce(avg_b, 0)) %>% 
+  as.data.frame()
+
+pilot4_data_cleaned
+
+pilot4indiv_cleaned <- pilot4_indiv %>%
   mutate(area_p4 = as.numeric(area_p4)) %>% 
   mutate(area_f = as.numeric(area_f)) %>% 
   mutate(area_b = as.numeric(area_b)) %>% 
@@ -25,102 +38,106 @@ pilot4_data_cleaned <- pilot4_data %>%
   mutate(area_b = coalesce(area_b, 0)) %>% 
   as.data.frame()
 
-pilot4_data_cleaned
+pilot4indiv_cleaned
 
 # Plotting bar graph of pilot 4 area data per treatment
 # P4 Progesterone graph
 pilot4_bar_p4 <- 
-  ggplot() + 
+  ggplot() +
   geom_bar(data = pilot4_data_cleaned,
-           aes(x = fct_inorder(reagent), 
-               y = (area_p4),
-               fill = as.factor(reagent)),
+           aes(x = fct_inorder(buffer),
+               y = avg_p4,
+               fill = reagent),
            stat = "identity",
            position = "dodge",
            width = 0.7,
-           na.rm = TRUE
+           na.rm = TRUE,
   ) +
-  geom_jitter(data = pilot4_data_cleaned,
-              aes(x = reagent,
-                  y = area_p4),
+  geom_jitter(data = pilot4indiv_cleaned,
+              aes(x = fct_inorder(buffer),
+                  y = area_p4,
+                  fill = reagent),
               stat = "identity",
-              position = position_jitter(height = 0,
-                                         width = 0)) +
+              position = position_jitterdodge()) +
   
   labs(title = "Pilot 4: 5mL Dichloromethane and Ethyl Acetate extraction of
-       Progesterone (P4) Steroid Hormone from Qiagen RNA Kit Flowthrough",
-       x = "Buffer Kit",
+       Progesterone (P4) Steroid Hormone from Qiagen Lysis Buffers",
+       x = "Buffer",
        y = "Area",
        fill = "Reagent"
   ) +
   
   theme_minimal() +
-  theme(plot.title = element_text(hjust = 0.5, size = 10)
+  theme(plot.title = element_text(hjust = 0.5, size = 10),
+        axis.text.x = element_text(angle = 80, hjust = 1)
   )  
 
 pilot4_bar_p4
 
 # B Corticosterone graph
-pilot4_bar_b <- 
-  ggplot() + 
-  geom_bar(data = pilot4_data_cleaned,
-           aes(x = fct_inorder(reagent), 
-               y = (area_b),
-               fill = as.factor(reagent)),
+pilot2_bar_b <- 
+  ggplot() +
+  geom_bar(data = pilot2_data_cleaned,
+           aes(x = fct_inorder(buffer),
+               y = avg_b,
+               fill = reagent),
            stat = "identity",
            position = "dodge",
            width = 0.7,
-           na.rm = TRUE
+           na.rm = TRUE,
   ) +
-  geom_jitter(data = pilot4_data_cleaned,
-              aes(x = reagent,
-                  y = area_b),
+  geom_jitter(data = pilot2indiv_cleaned,
+              aes(x = buffer,
+                  y = area_b,
+                  fill = reagent),
               stat = "identity",
-              position = position_jitter(height = 0,
-                                         width = 0)) +
+              position = position_jitterdodge()) +
   
   labs(title = "Pilot 4: 5mL Dichloromethane and Ethyl Acetate extraction of
-       Corticosterone (B) Steroid Hormone from Qiagen RNA Kit Flowthrough",
+       Corticosterone (B) Steroid Hormone from Qiagen Lysis Buffers",
        x = "Buffer Kit",
        y = "Area",
        fill = "Reagent"
   ) +
   
   theme_minimal() +
-  theme(plot.title = element_text(hjust = 0.5, size = 10)
+  theme(plot.title = element_text(hjust = 0.5, size = 10),
+        axis.text.x = element_text(angle = 80, hjust = 1)
   )  
 
-pilot4_bar_b
+pilot2_bar_b
 
 # F Cortisol graph
-pilot4_bar_f <- 
-  ggplot() + 
-  geom_bar(data = pilot4_data_cleaned,
-           aes(x = fct_inorder(reagent), 
-               y = (area_f),
-               fill = as.factor(reagent)),
+pilot2_bar_f <- 
+  ggplot() +
+  geom_bar(data = pilot2_data_cleaned,
+           aes(x = fct_inorder(buffer),
+               y = avg_f,
+               fill = reagent),
            stat = "identity",
            position = "dodge",
            width = 0.7,
-           na.rm = TRUE
+           na.rm = TRUE,
   ) +
-  geom_jitter(data = pilot4_data_cleaned,
-              aes(x = reagent,
-                  y = area_f),
+  geom_jitter(data = pilot2indiv_cleaned,
+              aes(x = buffer,
+                  y = area_f,
+                  fill = reagent),
               stat = "identity",
-              position = position_jitter(height = 0,
-                                         width = 0)) +
+              position = position_jitterdodge()) +
   
   labs(title = "Pilot 4: 5mL Dichloromethane and Ethyl Acetate extraction of
-       Cortisol (F) Steroid Hormone from Qiagen RNA Kit Flowthrough",
+       Cortisol (F) Steroid Hormone from Qiagen Lysis Buffers",
+       x = "Buffer Kit",
        x = "Buffer Kit",
        y = "Area",
        fill = "Reagent"
   ) +
   
   theme_minimal() +
-  theme(plot.title = element_text(hjust = 0.5, size = 10)
+  theme(plot.title = element_text(hjust = 0.5, size = 10),
+        axis.text.x = element_text(angle = 80, hjust = 1)
   )  
 
-pilot4_bar_f
+pilot2_bar_f
 
